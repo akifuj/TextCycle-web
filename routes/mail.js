85ff04da4c675f153b416b0e906fc5ed4629a742
AkiFuj <tessitetsusi@gmail.com>
2017-05-22 13:17:03 +0900

    modify sendgrid

--- routes/mail.js
+++ routes/mail.js
@@ -9,16 +9,21 @@ var subject = 'Hello World from the SendGrid Node.js Library!';
 var content = new helper.Content('text/plain', 'Hello, Email!');
 var mail = new helper.Mail(fromEmail, subject, toEmail, content);
 
-router.post('/', (req, res) => {
+router.post('/', (req, res, next) => {
   let error = {}
   const sgReq = sendgrid.emptyRequest({
     method: 'POST',
     path: '/v3/mail/send',
     body: mail.toJSON()
   });
-  sendgrid.API(sgReq)
-    .then(response => console.log(response.body))
-    .catch(err => console.log(error.response.statusCode));
+  sendgrid.API(sgReq, (err) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    res.json({ success: true })
+    return;
+  })
 });
 
 export default router;
