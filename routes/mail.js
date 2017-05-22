67b7367095485e296c16c9dc8ac6f1717b434650
AkiFuj <tessitetsusi@gmail.com>
2017-05-22 12:30:56 +0900

    modify sendgrid

--- routes/mail.js
+++ routes/mail.js
@@ -2,19 +2,24 @@ import express from 'express';
 let router = express.Router();
 var sendgrid = require('sendgrid')(process.env.SENDGRID_API_KEY);
 
-if (process.env.SENDGRID_API_KEY) {
-  console.log("ZZZZZZZZZ")
-}
+var helper = require('sendgrid').mail;
+var fromEmail = new helper.Email('tessitetsusi@gmail.com');
+var toEmail = new helper.Email('tessitetsusi@gmail.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'Hello, Email!');
+var mail = new helper.Mail(fromEmail, subject, toEmail, content);
 
 router.post('/', (req, res) => {
-  let errors = {};
-  sendgrid.send({
-    to: 'tessitetsusi@gmail.com',
-    from: 'tessitetsusi@gmail.com',
-    subject: 'Hello World',
-    text: 'Hello from SendGrid'
-  }, function(err, json) {
-    if (err) { res.status(500).json({ error: err }); }
+  error = {}
+  const sgReq = Sendgrid.emptyRequest({
+    method: 'POST',
+    path: '/v3/mail/send',
+    body: mail.toJSON()
+  });
+  Sendgrid.API(sgReq, (err) => {
+    if (err) {
+      res.status(500).json({ error: err })
+    }
     res.json({ success: true })
   });
 });
