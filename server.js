bd3925927de94cde7eb62c9fb8fbc33d0694b4ba
AkiFuj <tessitetsusi@gmail.com>
2017-05-28 15:07:44 +0900

    start ios server

--- server.js
+++ server.js
@@ -5,22 +5,26 @@ import mongoose from 'mongoose';
 import bodyParser from 'body-parser';
 import path from 'path'
 
+/*
 import posts from './routes/posts';
 import users from './routes/users';
 import auth from './routes/auth';
 import mail from './routes/mail';
+*/
 
-import user from './models/ios-user';
-import post from  './models/post'
+import iosUser from './models/ios-user';
+import post from  './models/post';
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+/*
 app.use('/api/posts', posts);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 app.use('/api/mail/send', mail);
+*/
 
 // GETリクエストに対処
 app.get('/api/ios/posts', (request, response) => {
