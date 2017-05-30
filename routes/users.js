19d278360a79ef4a5e81ddc77a61dc4bad1a8910
AkiFuj <tessitetsusi@gmail.com>
2017-05-30 09:07:37 +0900

    modify rotuer for ios

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,9 @@ router.post('/', (request, response) => {
 
 router.get('/:id', (request, response) => {
   user.findById(id, (err, user) => {
-    if (err) console.log(err); response.status(500).send();
+    if (err) {
+      console.log(err);
+      response.status(500).send();}
     else response.status(200).send(user);
   })
 })
